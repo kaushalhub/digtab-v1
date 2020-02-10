7099548f72b81587cfe784213f77c563a08f72fe
kaushalhub <40087944+kaushalhub@users.noreply.github.com>
2020-02-10 14:31:15 +0530

    connect-mysql

--- routes/pool.js
+++ routes/pool.js
@@ -1,10 +1,14 @@
 var mysql = require("mysql");
 
 const pool = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "123",
-  database: "digtab",
+ // host: "localhost",
+ // user: "root",
+  // password: "123",
+ // database: "digtab",
+ host: "us-cdbr-iron-east-04.cleardb.net",
+  user: "b8aabbd32e7b69",
+  password: "62a3b7d0",
+  database: "heroku_2f0ee115e587537",
   port: "3306",
   connectionLimit: 100,
   multipleStatements: true
